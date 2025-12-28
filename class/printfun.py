1738f3907a341432246df0c5c2481a33ea5327ce
jagdishpatil <jagdishpati8l2005@gmail.com>
2025-12-28 19:21:15 +0530

    Remove unused files and add print function examples in printfun.py

---
+++ class/printfun.py
@@ -0,0 +1,38 @@
+#print function in python
+print("hello jagdish")
+print('hello python')
+print(""" this is doc
+string this help  
+of print """)
+
+# print quates inside string
+print("hello \"this\" is me")
+
+a=10
+print(a)
+
+# name='jagdish'
+# print(name)
+
+# print multiple value
+x=10
+y=20
+print(x,y)
+
+# using sap.. parameter 
+# it decide how value saprate
+
+print('jagdish','nikhil','chetan',sep=" | ") 
+print('17','8','2005',sep="-")
+
+# using end parameter
+print("this is ",end="")
+print('jagdish')
+
+name="jagdish"
+email="jagdish243@gmail"
+print("hello my name is {0} & my email is {1}".format(name,email))
+print(f"my name is {name} & my email id is {email}")
+
+# printg exprisionn 
+print(10+2)
